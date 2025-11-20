681535ac59a3734c9c7ee177bed4cbc136512e63
carrocarros <carolinadagostin@gmail.com>
2025-11-20 16:38:46 -0500

    test..?

--- sudoku.py
+++ sudoku.py
@@ -1,1 +1,24 @@
+def start_screen():
+    '''
+    sudoku welcome screen.
+    displays welcome
+    select game mode
+    ***WIP***
+    '''
 
+def game_won_screen():
+    '''
+    sudoku game won screen.
+    happens when user wins the game! yay!
+    dislays 'game won!' and has exit button that ends the program
+    ***WIP***
+    '''
+
+
+def game_over_screen():
+    '''
+    sudoku game over screen.
+    idk how you get a game over on sudoku but alright
+    dislays 'game over :(!' and has restart button that ends the program
+    ***WIP***
+    '''
