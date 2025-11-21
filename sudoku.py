b973100fee984aba834257c964862bd76d729110
carrocarros <carolinadagostin@gmail.com>
2025-11-20 19:13:47 -0500

    progress on start screen

--- sudoku.py
+++ sudoku.py
@@ -1,18 +1,16 @@
-import pygame
+import pygame, sys
 
+#frank here, moved init() up so that the other funtions work
+pygame.init()
+screen = pygame.display.set_mode((640, 512))
 
 color_beige = (255, 239, 204)
 color_dark_brown = (36, 27, 20)
 color_light_brown = (173, 106, 57)
 
+title_font = pygame.font.SysFont("Arial", 32) #placeholder style
+subtitle_font = pygame.font.SysFont("Arial", 20) #placeholder style
 
-def main():
-
-
-   pygame.init()
-   screen = pygame.display.set_mode((640, 512))
-   clock = pygame.time.Clock()
-   running = True
 
 def start_screen():
     '''
@@ -21,6 +19,16 @@ def start_screen():
     select game mode
     ***WIP***
     '''
+    screen.fill(color_beige) #switch to an image if have the time
+
+    start_surf = title_font.render("Sudoku!", True, color_dark_brown)
+    screen.blit(start_surf, (0, 0)) #placeholder coordinates
+
+    sub_start_surf = subtitle_font.render("Please select difficulty:", True, color_dark_brown)
+    screen.blit(sub_start_surf, (100, 100)) #placeholder coordinates
+
+
+
 
 def game_won_screen():
     '''
@@ -38,3 +46,13 @@ def game_over_screen():
     dislays 'game over :(!' and has restart button that ends the program
     ***WIP***
     '''
+
+
+
+
+def main():
+
+
+
+    clock = pygame.time.Clock()
+    running = True
